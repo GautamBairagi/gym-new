66604cf50b2e6520203a8bd6e750fb1cde16fad8
Gopal Patil <patilgopal5550@gmail.com>
2025-12-04 18:02:43 +0530

    lfkjd

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ export const pool = mysql
   .createPool({
     host: "localhost",
     user: "root",
-    password: "Gopal_1820",
+    password: "",
     database: "gym_db",
     port: 3306,
     waitForConnections: true,
